b64034185cdc61af31471da855de380366836cb4
Ole Kristian Aune <okaune@outlook.com>
2018-08-30 13:19:12 +0200

    Fixed so age is always right

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import differenceInYears from 'date-fns/difference_in_years'
 
 import ResumeBlock from '../components/ResumeBlock';
 
@@ -16,7 +17,7 @@ const Main = props => {
       <section id="about">
         <h1>Om meg</h1>
         <p>
-          Jeg er en student på {new Date().getFullYear() - 1995} år som elsker å engasjere meg.
+          Jeg er en student på {differenceInYears(new Date(), new Date(1995, 4, 8))} år som elsker å engasjere meg.
           Er med på å arrangere alt fra sosiale arrangementer til kurs, og er veldig opptatt av å arbeide for godt samhold mellom studentene.
           Fikk tidlig interesse for datamaskiner, og laget min første nettside da jeg gikk i 6. klasse.
           Derfra har interessen bare økt, og jeg prøver aktivt å holde meg oppdatert på alt det nye som skjer i bransjen.
