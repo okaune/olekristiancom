f9418d0d6ace626fe315e74eb721020508354f51
Ole Kristian Aune <okaune@outlook.com>
2018-12-28 03:56:48 +0100

    Added testing step

--- src/test/Main.test.js
+++ src/test/Main.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+
 import Main from '../pages/Main';
 
+configure({adapter: new Adapter()});
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<Main />);
 });
