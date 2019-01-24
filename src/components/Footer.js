bedea786222ee21bea7f0c3588a215c7a28497b7
Ole Kristian Aune <okaune@outlook.com>
2019-01-24 17:51:47 +0100

    Footer now gets current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import getYear from 'date-fns/get_year'
 
 import '../styles/Footer.scss'
 
 const Footer = props => (
     <footer className="footer">
         <p className="footer__links"><a href="mailto:post@olekristian.com">E-post</a>&nbsp;&nbsp;&nbsp;&middot;&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/ole.kristian.aune">Facebook</a>&nbsp;&nbsp;&nbsp;&middot;&nbsp;&nbsp;&nbsp;<a href="https://www.linkedin.com/in/olekristianaune/">LinkedIn</a>&nbsp;&nbsp;&nbsp;&middot;&nbsp;&nbsp;&nbsp;<a href="https://github.com/okaune">GitHub</a></p>
-        <p className="footer__copy">&copy; 2018 Ole Kristian Aune. Alle rettigheter forbeholdes.</p>
+        <p className="footer__copy">&copy; {getYear(new Date())} Ole Kristian Aune. Alle rettigheter forbeholdes.</p>
     </footer>
 )
 
